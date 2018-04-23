4b5771b40861aa85de98e11954cace8c74dacecd
Rushabh Dadbhawala <rushabh.dadbhawala@continuum.net>
2018-04-23 11:21:01 +0530

    static logger

---
+++ src/LogDesign/LogDesign/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogDesign
+{
+    public class Logger
+    {
+		public static void Log(string message)
+		{
+			using (StreamWriter sw = new StreamWriter(new FileStream("log.log", FileMode.Append, FileAccess.Write)))
+			{
+				sw.WriteLine(message);
+			}
+		}
+    }
+}
